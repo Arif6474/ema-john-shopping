5fe794ea03db22b7e82a26a0b16495bb7b8b3c77
Arif6474 <arifulislam64743@gmail.com>
2022-03-24 13:03:08 +0600

    added local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+         total = total + product.price;
+         shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.10).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax)  ;
     return (
-        <div>
+        <div className="cart">
             <h3>Order summary</h3>
-                <h4>Selected Items: {cart.length}</h4>
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: ${shipping}</p>
+                <p>Tax: ${tax}</p>
+                <h4>Grand Total: {grandTotal}</h4>
         </div>
     );
 };
